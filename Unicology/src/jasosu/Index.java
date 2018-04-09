ee8b9bcae725f108acae5fe4e76714c0ddf104c7
Mary <Mary@DESKTOP-B30OSM9>
2018-04-09 15:49:00 +0900

    웹크롤링 완료

---
+++ Unicology/src/jasosu/Index.java
@@ -0,0 +1,139 @@
+package jasosu;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+public class Index {
+      
+	// 크롤링할 메인페이지 sub_url변수에 저장
+	static String sub_url = "http://www.jobkorea.co.kr/starter/PassAssay?FavorCo_Stat=0&Pass_An_Stat=0&OrderBy=0&EduType=0&WorkType=0&isSaved=0&Page=";
+	static int page = 1;
+	static String url = sub_url + page;        
+  
+	public static void main(String[] args) throws IOException{
+		// 페이지 카운팅 변수
+		int count = 0;
+		// field 변수 index
+		int fi = 0;
+		// field 카운팅 변수
+		int field_count = 0;
+		// title 카운팅 변수
+		int title_count = 0;
+		//contents 카운팅 변수
+		int contents_count = 0;
+		
+		JasosuDAO jDao = new JasosuDAO();
+		// field 배치 저장위해 ArrayList객체 생성
+		ArrayList<String> field = new ArrayList<>();
+              
+		while(page <= 2) {
+				// 페이지 카운팅을 위해 1증가
+				count += 1;
+				
+				// 메인페이지 url을 Jsoup라이브러리 Document타입 root_url변수로 저장
+				Document root_url = Jsoup.connect(url).get();
+               
+				// root_url변수에서 .linkArray > .field 태그만 선택해 Elements배열타입 field_Tag변수에 저장 [분야 크롤링]
+				Elements field_Tag = root_url.select(".linkArray > .field");
+				
+				// root_url변수에서 p.tit > a 태그만 선택해 Elements배열타입 contents_Tag변수에 저장 [태그에 href 속성값을 가져오기위해]
+				Elements contents_Tag = root_url.select("p.tit > a");
+                
+				// 크롤링 시작페이지 출력
+				System.out.println();
+				System.out.println("========================================" + page + "페이지========================================");
+				System.out.println();
+                
+				// field_Tag배열 변수의 index만큼 반복
+				for(int i = 0; i < field_Tag.size(); i++) {
+                    
+					// 분야는 배치로 ArrayList에 저장
+					
+					// shareField 배열 변수의 0번째 index는 경력, 1번째 index는 분야, 분야만 추출하기 위해 조건문 사용  
+					if( i%2 == 1 ) {						
+						// field 카운팅을 위해 field_count 1증가
+						field_count += 1;
+						
+						// field_Tag변수의 분야만 추출하여 Element타입 shareField 변수에 저장								
+						Element shareField = field_Tag.get(i);						
+						
+						// field 변수에 shareField에 담긴 텍스트만 저장
+						field.add(shareField.text());	
+					}					
+				}
+				// 한페이지의 분야값 field 배열변수에  모두 저장
+				
+				for(Element element:contents_Tag) {					
+					//element(contents_Tag)에 href속성값을 main_sub_url에 저장
+					String main_sub_url = element.attr("href");
+                     
+					// main_sub_url 변수값에 "/starter/PassAssay/View/"이 포함될경우 아래 조건문 이동
+					if(main_sub_url.contains("/starter/PassAssay/View/")) {
+						// "http://www.jobkorea.co.kr" + main_sub_url을 Jsoup라이브러리 Document타입 doc변수로 저장
+						Document doc = Jsoup.connect("http://www.jobkorea.co.kr" + main_sub_url).get();
+						System.out.println("현재 접속할 url주소 : " + "http://www.jobkorea.co.kr" + main_sub_url);
+                            
+						// doc변수에서 button > span.tx태그만 선택해 Elements배열타입 title_Tag변수에 저장 [제목 크롤링]
+						Elements title_Tag = doc.select("button > span.tx");
+						// doc변수에서 dd > .tx태그만 선택해 Elements배열타입 body_Tag변수에 저장 [내용 크롤링]
+						Elements body_Tag = doc.select("dd > .tx");                        
+						
+						// title_Tag배열 변수의 index 만큼 반복
+						for(int i = 0; i < title_Tag.size(); i++) {
+							// 반복문 시작시 title, contents변수 초기화
+							String title=null;
+							String contents = null;	
+							
+							// title과 contents 카운팅을 위해 1씩증가
+							title_count += 1;
+							contents_count += 1;
+							
+							//title_Tag의 j번째 인덱스의 텍스트만 title변수에 저장
+							title = title_Tag.get(i).text();
+							//body_Tag의 j번째 인덱스의 텍스트만 contents변수에 저장
+							contents = body_Tag.get(i).text();
+							
+							// title_count행 출력
+							System.out.println();
+							System.out.println("분야 : " + field.get(fi));
+							System.out.println("제목 : " + title);
+							System.out.println("내용 : " + contents);
+	                        System.out.println();	                        
+							
+	                        // jDto 에 변수 저장 [ field.get(fi) : 배열변수의 인덱스를 지정시 String 타입으로 변경 ]
+	                        JasosuDTO jDto = new JasosuDTO(field.get(fi), title, contents);
+	                        
+	                        // DAO에 Jaso_Insert클래스에 jDto를 매게변수로 보냄
+							jDao.Jaso_Insert(jDto);
+							
+							// 몇번째 행인지 출력
+							System.out.println("---------------------------------------------------------------------- " + title_count + " 행 저장 완료---------------------------------------------------------------------");
+							
+						}
+					}
+					// field 다음 인덱스를 가져오기위해 fi변수 1증가
+					fi += 1;
+				}
+				// 저장 완료된 페이지 출력
+				System.out.println();
+				System.out.println("=================================== " + page + "페이지 저장 완료 ===================================");
+				System.out.println();
+				
+				// 다음 메인 페이지를 가져오기위해 page값 1증가
+				page ++;                    
+				url = sub_url + page;
+		}
+		
+		// 각 컬럼별 총 저장개수 출력
+		System.out.println("--------------------------------------------------------- 총 저장된 분야 변수 인덱스 : " + field_count + " --------------------------------------------------------");
+		System.out.println("--------------------------------------------------------- 총 저장된 제목 변수 인덱스 : " + title_count + " --------------------------------------------------------");
+		System.out.println("--------------------------------------------------------- 총 저장된 내용 변수 인덱스 : " + contents_count + " --------------------------------------------------------");
+		System.out.println("----------------------------------------------------------- 총" + count + "페이지를 저장했습니다. -----------------------------------------------------------");
+		System.out.println();         
+         
+	}
+}
---
+++ Unicology/src/jasosu/JasosuDTO.java
@@ -0,0 +1,55 @@
+package jasosu;
+
+public class JasosuDTO {
+	
+	private String jaso_no;
+	private String jaso_field;
+	private String jaso_title;
+	private String jaso_contents;
+	
+	public JasosuDTO() {
+		
+	}
+
+	public JasosuDTO(String jaso_field, String jaso_title, String jaso_contents) {
+		super();
+		this.jaso_field = jaso_field;
+		this.jaso_title = jaso_title;
+		this.jaso_contents = jaso_contents;
+	}
+	
+	public String getJaso_field() {
+		return jaso_field;
+	}
+
+	public void setJaso_field(String jaso_field) {
+		this.jaso_field = jaso_field;
+	}
+
+	public String getJaso_no() {
+		return jaso_no;
+	}
+
+	public void setJaso_no(String jaso_no) {
+		this.jaso_no = jaso_no;
+	}
+
+	public String getJaso_title() {
+		return jaso_title;
+	}
+
+	public void setJaso_title(String jaso_title) {
+		this.jaso_title = jaso_title;
+	}
+
+	public String getJaso_contents() {
+		return jaso_contents;
+	}
+
+	public void setJaso_contents(String jaso_contents) {
+		this.jaso_contents = jaso_contents;
+	}
+	
+	
+	
+}
